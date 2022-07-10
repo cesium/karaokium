f2a3eed104f4bdf619f209a2c4483a4e4e228c32
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2022-07-10 13:11:43 +0100

    Add tailwind (#30)
    * Add tailwind
    
    * Format
    
    * Implement suggestions

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,17 @@ config :karaokium, Karaokium.Mailer, adapter: Swoosh.Adapters.Local
 # Swoosh API client is needed for adapters other than SMTP.
 config :swoosh, :api_client, false
 
+config :tailwind,
+  version: "3.1.4",
+  default: [
+    args: ~w(
+    --config=tailwind.config.js
+    --input=css/app.css
+    --output=../priv/static/assets/app.css
+  ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
 # Configure esbuild (the version is required)
 config :esbuild,
   version: "0.14.43",
